205e86271a35e774e82baf66fe98a77965c81b81
rooheen garg <rooheen.garg@1mg.com>
2020-07-19 16:26:15 +0530

    savepoint

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CameraFeed from './components/CameraFeed';
+import './style.scss';
+
+const App = (props) => {
+const wordsArray = ['Candid', 'Crazy', 'Capture', 'Catchy']; 
+const uploadImage = async file => {
+    const formData = new FormData();
+    formData.append('file', file);
+};
+
+    return (
+        <div className="App">
+            <h1 className="heading">Image capture test</h1>
+            <CameraFeed sendFile={uploadImage} />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
