b61554a6bf1c4ed35c922488302fde4b7917f5b3
rooheen garg <rooheen.garg@1mg.com>
2020-07-19 16:39:47 +0530

    build folder

---
+++ build/precache-manifest.eb9030fc070f2a2dba64a0702c48b00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4949d8a7f3e4834d2d07ec44712d5bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31576d50005b63ad06eb",
+    "url": "/static/css/main.e8b24619.chunk.css"
+  },
+  {
+    "revision": "25c3cf578b553e36e43c",
+    "url": "/static/js/2.c2543143.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2543143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31576d50005b63ad06eb",
+    "url": "/static/js/main.61e9ffe5.chunk.js"
+  },
+  {
+    "revision": "38357aeeed8bc62453a5",
+    "url": "/static/js/runtime-main.473275ef.js"
+  }
+]);
